f29d32cc8218ff64cc55c8c0faf9a1302005fb1e
Deanna Yee <deannayee0108@gmail.com>
2016-10-23 21:20:47 -0700

    create navigation, footer start on home page add images

---
+++ js/footer.js
@@ -0,0 +1,16 @@
+function createFooter(){
+	document.write('<footer>');
+	document.write('<div id="container">');
+	document.write('<div class="footerInfo" id="dateAndTime">');
+	document.write('<p class="fallInfo" id="fall">Fall Semester:</p>');
+	document.write('<p class="fallInfo" id="dates">Dates: Oct.13,2016 - Dec.15,2016</p>');
+	document.write('<p class="fallInfo" id="thursday">Thursdays: 3:30p.m. - 5:30p.m.</p>');
+	document.write('</div>');
+	document.write('<div class="footerInfo" id="location">');
+	document.write('<p class="locationInfo" id="sanCarlos"><a href="http://smcl.org/locations/1S/">San Carlos Library</a></p>');
+	document.write('<p class="locationInfo" id="address">610 Elm Street</p>');
+	document.write('<p class="locationInfo" id="cityInfo">San Carlos, CA 94070</p>');
+	document.write('</div>');
+	document.write('</div>');
+	document.write('</footer>');
+}
---
+++ js/navigation.js
@@ -0,0 +1,11 @@
+function createNavBar(){
+	document.write('<nav>');
+	document.write('<ul>');
+	document.write('<li><a href="#">Home</a></li>');
+	document.write('<li><a href="/events">Events</a></li>');
+	document.write('<li><a href="/guides">Guides</a></li>');
+	document.write('<li><a href="/sessiosSlides">Session Slides</a></li>');
+	document.write('<li><a href="/impactProject">Impact Project</a></li>');
+	document.write('<ul>');
+	document.write("</nav>");
+}
