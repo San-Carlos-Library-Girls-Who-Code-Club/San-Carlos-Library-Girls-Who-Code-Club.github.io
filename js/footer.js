db6b79270d266c72043c2b2a7e0605b8b7a11a63
Deanna Yee <deannayee0108@gmail.com>
2016-10-23 21:56:11 -0700

    change nav and footer

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ function createFooter(){
 	document.write('<div id="container">');
 	document.write('<div class="footerInfo" id="dateAndTime">');
 	document.write('<p class="fallInfo" id="fall">Fall Semester:</p>');
-	document.write('<p class="fallInfo" id="dates">Dates: Oct.13,2016 - Dec.15,2016</p>');
+	document.write('<p class="fallInfo" id="dates">Dates: Oct.13, 2016 - Dec.15, 2016</p>');
 	document.write('<p class="fallInfo" id="thursday">Thursdays: 3:30p.m. - 5:30p.m.</p>');
 	document.write('</div>');
 	document.write('<div class="footerInfo" id="location">');
