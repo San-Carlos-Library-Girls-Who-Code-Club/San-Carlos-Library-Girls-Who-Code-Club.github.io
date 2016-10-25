c7e84ac068d19373eacb0f37021d59da43a381e0
Deanna Yee <deannayee0108@gmail.com>
2016-10-25 12:34:37 -0700

    changed guides to resources

--- js/navigation.js
+++ js/navigation.js
@@ -3,7 +3,7 @@ function createNavBar(){
 	document.write('<ul>');
 	document.write('<li><a href="#">Home</a></li>');
 	document.write('<li><a href="/events">Events</a></li>');
-	document.write('<li><a href="/guides">Guides</a></li>');
+	document.write('<li><a href="/guides">Resources</a></li>');
 	document.write('<li><a href="/sessiosSlides">Session Slides</a></li>');
 	document.write('<li><a href="/impactProject">Impact Project</a></li>');
 	document.write('<ul>');
