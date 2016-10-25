1ffc68b9d30a83f405c0b096109c31362c13c1eb
Deanna Yee <deannayee0108@gmail.com>
2016-10-25 12:38:07 -0700

    spelling of session changed

--- js/navigation.js
+++ js/navigation.js
@@ -4,7 +4,7 @@ function createNavBar(){
 	document.write('<li><a href="#">Home</a></li>');
 	document.write('<li><a href="/events">Events</a></li>');
 	document.write('<li><a href="/guides">Resources</a></li>');
-	document.write('<li><a href="/sessiosSlides">Session Slides</a></li>');
+	document.write('<li><a href="/sessionSlides">Session Slides</a></li>');
 	document.write('<li><a href="/impactProject">Impact Project</a></li>');
 	document.write('<ul>');
 	document.write("</nav>");
